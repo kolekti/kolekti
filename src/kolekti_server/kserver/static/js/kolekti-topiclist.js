a354629617a6e0d212edaae4423b942843fa9aa3
Stéphane Bonhomme <stephane@exselt.com>
2015-01-22 21:54:18 +0100

    topic creation fonctionality

--- src/kolekti_server/kserver/static/js/kolekti-topiclist.js
+++ src/kolekti_server/kserver/static/js/kolekti-topiclist.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
 
     $('#btn_create').on("click", function(e) {
 	var folder = $('.browserparent').data('path');
-	var file = $('#new_name').val()
+	var filename = $('#new_name').val()
 	$.post('/topics/create/',
-	       {'modelpath': $('#template').data('path'),
-		'topicpath': folder + "/" + file
+	       {
+		   'model': $('.label-tpl').data('tpl'),
+		   'topicpath': folder + "/" + filename
 	       })
 	    .success(
 		window.location.reload()
 	    )
     })
     
-    $('#template').append();
 
     kolekti_browser({'root':'/sources/'+kolekti.lang+'/topics',
 		     'parent':".browserparent",
@@ -26,4 +26,48 @@ $(document).ready(function() {
 		window.open('/topics/edit/?topic='+path);
 	    }
 	)
+    $.get('/topics/templates/?lang='+kolekti.lang)
+    .success(function(data) {
+	$('<div>',{ 'class':'btn-group',
+		    'html':[$('<button>',{
+			'type':'button',
+			'class':'btn btn-default',
+			'data-toggle':'dropdown',
+			'role':'button',
+			'aria-expanded':'false',
+			'html':[$('<span>',{
+			    'class':'label-tpl',
+			    'data-tpl':data[0],
+			    'html':data[0]}),
+				' ',
+				$('<span>',{
+				    'class':'caret'})
+			       ]}),
+			    $('<ul>',{
+				'class':'dropdown-menu',
+				'role':'menu',
+				"id":"tpllist"
+			    })]
+		   }
+	 ).appendTo("#tpl");
+	$.each(data, function(i,v) {
+	    $('<li>',{
+		'class':'presentation',
+		'html':$("<a>",{
+		    'href':"#",
+		    'class':'tpl-item',
+		    'data-tpl':v,
+		    'html':v
+		})
+	    }).appendTo('#tpllist');
+	})
+    })
+    
+    $('#tpl').on('click', '.tpl-item',function(e){
+	e.preventDefault();
+	$('.label_tpl').html($(this).data('tpl'))
+	$('.label_tpl').data('tpl',$(this).data('tpl'));
+    })
+	
+
 })
