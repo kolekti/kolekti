a2deea5bc009340c353f3edcc7f9d18df8c10c53
Stephane Bonhomme <stephane@exselt.com>
2015-02-18 12:12:51 +0100

    automatically copy setup to F/ directory

--- src/setup-pyinstaller.bat
+++ src/setup-pyinstaller.bat
@@ -1,4 +1,9 @@
-set PYTHONPATH=C:\Users\waloo\Desktop\kolekti\kolekti\src\kolekti_server;C:\Users\waloo\Desktop\kolekti\kolekti\src
+@set PYTHONPATH=C:\Users\waloo\Desktop\kolekti\kolekti\src\kolekti_server;C:\Users\waloo\Desktop\kolekti\kolekti\src
 
-#pyinstaller -w --name kolekti_server kolekti_server\server.py
-pyinstaller -y  kolekti_server.spec
+@rem pyinstaller -w --name kolekti_server kolekti_server\server.py
+@pyinstaller -y  kolekti_server.spec
+@"C:\Program Files\Inno Setup 5\ISCC.exe" C:\Users\waloo\Desktop\kolekti\kolekti\src\setup.iss
+
+@for /f "tokens=1-4 delims=/ " %%G in ('date /t') do set mmddyyyy=%%I_%%H_%%G
+
+@copy C:\Users\waloo\Desktop\kolekti\kolekti\src\Output\setup-kolekti_0.7.exe F:\kolekti\setup-kolekti_0.7_%mmddyyyy%.exe
