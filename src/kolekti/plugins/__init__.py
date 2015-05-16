0e83b5066797c2ef089e26fbc094fd13998eed7e
Stéphane Bonhomme <stephane@exselt.com>
2015-05-16 20:33:06 +0200

    new plugin loading method

--- src/kolekti/plugins/__init__.py
+++ src/kolekti/plugins/__init__.py
@@ -1,11 +1,16 @@
-import imp
-import os
-import sys
-import logging
-def getPlugin(plugin, path):
-    logging.debug('plugin import %s'%plugin)
-    d = os.path.dirname(__file__)
-    pm = imp.load_source('kolekti.plugins.'+plugin, os.path.join(d,plugin+'.py'))
-    pl = getattr(pm, "plugin")
-    return pl(path)
+#import imp
+#import os
+#import sys
+#import logging
+
+import chm
+import hlp
+import WebHelp5
+
+#def getPlugin(plugin, path):
+#    logging.debug('plugin import %s'%plugin)
+#    d = os.path.dirname(__file__)
+#    pm = imp.load_source('kolekti.plugins.'+plugin, os.path.join(d,plugin+'.py'))
+#    pl = getattr(pm, "plugin")
+#    return pl(path)
 
