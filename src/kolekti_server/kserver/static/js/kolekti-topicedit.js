796e3e6d8c7d384fc9511016984919c9f2f851e5
Stéphane Bonhomme <stephane@exselt.com>
2015-06-12 08:00:33 +0200

    conditions dialog in ckeditor

--- src/kolekti_server/kserver/static/js/kolekti-topicedit.js
+++ src/kolekti_server/kserver/static/js/kolekti-topicedit.js
@@ -1,11 +1,38 @@
 $(document).ready( function () {
-    var editor = CKEDITOR.replace( 'editor1', {customConfig: '/static/ckeditor_config_topic.js'});
+    
+    var editor = CKEDITOR.replace( 'editor1', {
+	autoGrow_onStartup : true,
+	contentsCss : '/criteria.css',
+	extraPlugins : 'codemirror,textselection,conditions',
+	extraAllowedContent : 'span *(*)',
+	entities : false,
+	toolbar_Full : [
+	    { name: 'document',    groups: [ 'mode', 'document', 'doctools' ], items: [ 'Save', 'Preview', 'Source', 'Print' ] },
+	    { name: 'clipboard',   groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', 'Undo', 'Redo' ] },
+	    { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', 'SelectAll', 'Scayt' ] },
+	    { name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar' ] },
+	    { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+	    { name: 'about', items: [ 'About' ] },
+	    '/',
+	    { name: 'tools', items: [ 'ShowBlocks' ] },
+	    { name: 'styles', items: [ 'Format' ] },
+	    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'RemoveFormat' ] },
+	    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ], items: [ 'NumberedList', 'BulletedList', 'CreateDiv', 'conditions'] }
+	],
+	toolbar:"Full"
+
+
+    });
+
+    //	customConfig: '/static/ckeditor_config_topic.js'});
+    
     var savestate = 0;
 
     editor.on( 'instanceReady', function(event){ 					
 	event.editor.execCommand( 'maximize'); 
 	event.editor.commands.save.disable();
     });
+    
     editor.on( 'save', function(event){ 
 	$.ajax({
 	    url:window.location.pathname+window.location.search,
