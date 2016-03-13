230d699baad56ba2c4d79321ac3380bf6079543f
Stéphane Bonhomme <stephane@exselt.com>
2016-03-13 11:54:30 +0100

    prevent redrwing existing maps

--- src/kolekti_server/kserver/static/components/js/map.js
+++ src/kolekti_server/kserver/static/components/js/map.js
@@ -4,23 +4,23 @@ $(document).ready(function() {
     $('.section-content.collapse').on('shown.bs.collapse', function(e) {
 	if ($(e.target).hasClass('section-content')) {
 	    $(e.target).find('.leafletmap').each(function() {
-		console.log('display map');
-		var geojson = $(this).data('geojson').results.bindings[0].geojson.value	
-		var geodata = JSON.parse(geojson)
-		console.log(geodata)
-		
-		var map = L.map($(this).get(0));
-		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		    maxZoom: 18,
-		    attribution: 'Map data ©<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		    id: 'mapbox.streets'
-		}).addTo(map);
-		
-		var myLayer = L.geoJson(geodata);
-		myLayer.addTo(map) ;
-		map.fitBounds(myLayer.getBounds()) ;
+		if (! $(this).find('div').length) {
+		    var geojson = $(this).data('geojson').results.bindings[0].geojson.value	
+		    var geodata = JSON.parse(geojson)
+		    
+		    var map = L.map($(this).get(0));
+		    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			attribution: 'Map data ©<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'mapbox.streets'
+		    }).addTo(map);
+		    
+		    var myLayer = L.geoJson(geodata);
+		    myLayer.addTo(map) ;
+		    map.fitBounds(myLayer.getBounds()) ;
+		}
 	    });
 	}
     })
