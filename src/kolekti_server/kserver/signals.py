73a3adfa95d84d59daf08686a50bc51a93463368
Stéphane Bonhomme <stephane@exselt.com>
2016-05-30 15:30:35 +0200

    adds project creation on user validation signal

--- src/kolekti_server/kserver/signals.py
+++ src/kolekti_server/kserver/signals.py
@@ -10,8 +10,9 @@ logger = logging.getLogger('kolekti.'+__name__)
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from registration.signals import user_activated
 
-from kserver.models import UserProject
+from kserver.models import Project,UserProject
 
 from kolekti.synchro import SVNProjectManager
 
@@ -30,3 +31,21 @@ def post_save_userproject_callback(sender, **kwargs):
             SVNProjectManager(projectsroot, username = username).checkout_project(instance.project.directory, url)
         except:
             logger.exception('error during checkout')
+
+
+@receiver(user_activated)
+def user_activated_callback(sender, user, request, **kwarg):
+    user.userprofile
+    project = Project.objects.get(name='ecorse')
+    up = UserProject(user = user,
+                project = project,
+                is_saas = True,
+                is_admin = False)
+                             
+    up.save()
+    logger.debug('--------------- set kolekti project')
+    projectpath = os.path.join(settings.KOLEKTI_BASE, request.user.username, project.directory)
+    self.set_project(projectpath)
+    logger.debug('--------------- activate')
+    self.project_activate(up)
+    logger.debug('--------------- end ')
