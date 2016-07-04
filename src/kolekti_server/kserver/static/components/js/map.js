abc2619a8c4b2d10776694757fc195e39eb190c8
Stéphane Bonhomme <stephane@exselt.com>
2016-07-04 10:07:53 +0200

    adds char and map on starred topic displayed in home page

--- src/kolekti_server/kserver/static/components/js/map.js
+++ src/kolekti_server/kserver/static/components/js/map.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
     console.log('init map componenent');
 
+    // display map on report main page for starred topics
+    $('.alaune .leafletmap').each(function() {
+	if (! $(this).find('div').length) {
+	    var geojson = $(this).data('geojson').results.bindings[0].geojson.value	
+	    var geodata = JSON.parse(geojson)
+	    
+	    var map = L.map($(this).get(0));
+	    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: 'Map data ©<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'mapbox.streets'
+	    }).addTo(map);
+	    
+	    var myLayer = L.geoJson(geodata);
+	    myLayer.addTo(map) ;
+	    map.fitBounds(myLayer.getBounds()) ;
+	}
+    });
+
+    
     // deroulement de la section (affichage des topics)
     $('.section-content.collapse').on('shown.bs.collapse', function(e) {
 	if ($(e.target).hasClass('section-content')) {
