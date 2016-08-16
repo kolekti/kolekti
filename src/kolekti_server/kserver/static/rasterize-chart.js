c5d2699aa76ab08fc0c3c0c0978b488873862cf4
Stéphane Bonhomme <stephane@exselt.com>
2016-08-16 17:30:09 +0200

    sets chart width

--- src/kolekti_server/kserver/static/rasterize-chart.js
+++ src/kolekti_server/kserver/static/rasterize-chart.js
@@ -55,7 +55,7 @@ if (system.args.length < 3 || system.args.length > 5) {
 		var svg = d3.select(window.document.body).select('div');
 		var svglist = window.document.getElementsByTagName('div');
 		try {
-		    window.drawchart(svglist[0], false, 725);
+		    window.drawchart(svglist[0], false, 600);
 		}
 		catch(e) {
 		    $('body').html('Erreur a la génération du graphique');
