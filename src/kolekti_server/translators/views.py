6a1df0267162dfb249af88aef4b5242fad93890b
Stéphane Bonhomme <stephane@exselt.com>
2018-05-07 19:00:39 +0200

    prevent cache project files in translator interface

--- src/kolekti_server/translators/views.py
+++ src/kolekti_server/translators/views.py
@@ -17,6 +17,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives
+from django.utils.cache import add_never_cache_headers
 
 import logging
 logger = logging.getLogger('kolekti.'+__name__)
@@ -411,7 +412,9 @@ class TranslatorsStaticView(TranslatorsMixin, View):
             projectpath = os.path.join(settings.KOLEKTI_BASE, self.request.user.username, project)
             path = "/releases/"+release+"/"+path
             logger.debug("serve %s/%s", path, projectpath)
-            return serve(request, path, projectpath)
+            response = serve(request, path, projectpath)
+            add_never_cache_headers(response)
+            return response
         except:
             logger.exception('static error')
             return HttpResponse(status=500)            
